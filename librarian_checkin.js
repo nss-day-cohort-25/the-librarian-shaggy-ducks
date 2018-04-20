0a8416e8d6911d236c4796b9f4aa5c24123893db
MrErin <erinmeaker@gmail.com>
2018-04-20 14:54:50 -0500

    finished librarian checkin and checkout

--- librarian_checkin.js
+++ librarian_checkin.js
@@ -1,9 +1,16 @@
-//need to name function with camel case not underscore
 //input: findbook function (by title), customer object
 //output: change book object due date, change checked out status on book object, remove from customer bookshelf
 //if overdue, customer owes $5
 
+const librarianCheckIn = (book) => {
+	if (book.checkedOut === false) {
+		console.log('this book is in the library')
+		return false
+	} else {
+		book.checkedOut = false
+		book.dueDate = ''
+		console.log('book received. thank you!')
+		return book
+	}
 
-//if 
-
-
+}
