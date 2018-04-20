1d732566033974d75db22b6e4cc176d23a4a9b7a
binauralbeat <binauralbeat001@gmail.com>
2018-04-20 15:41:00 -0500

    created some books and customers

--- customers.js
+++ customers.js
@@ -1,1 +1,4 @@
 let Levi = customer("Levi", "Schubert", "Fantasy", "127.0.0.1" )
+let Dan = customer("Dan", "Beecroft", "Satire", "Beverly Hills 90210")
+let Erin = customer("Erin", "Meaker", "","")
+let Hayley = customer("Hayley", "Landsberg","","")
