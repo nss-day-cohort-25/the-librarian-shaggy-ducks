90cf0113199745c6e631db08ae7ca60ad59cec54
Hayley Landsberg <hayleyheath@gmail.com>
2018-04-23 10:32:08 -0500

    Added DOM components into main.js and added style to page

---
+++ main.js
@@ -0,0 +1,23 @@
+const storyRef = document.querySelector("#storyRef")
+
+
+for (let i = 0; i < storyArray.length; i++) {
+    if (!storyArray[i].isArray()) {
+        const storySection = document.createElement("section")
+        storySection.classList.add("storySection")
+        storySection.textContent = `${storyArray[i]}`
+        storyRef.appendChild(storySection)
+    } else {
+        const storySection = document.createElement("section")
+        storySection.classList.add("storySection genre")
+        const storyUl = document.createElement("ul")
+        storySection.textContent = `${storyArray[i][0]}`
+        for (let j = 1; j < storyArray[i].length; j++) {
+            const storyLi = document.createElement("li")
+            storyLi.textContent = `${storyArray[i][j]}`
+            storyUl.appendChild(storyLi)
+        }
+        storySection.appendChild(storyUl)
+        storyRef.appendChild(storySection)
+    }
+}
